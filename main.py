758a16f8a132e532a31b4226564687a6778c1d28
Ben-Edwards44 <94799394+Ben-Edwards44@users.noreply.github.com>
2022-10-02 15:27:01 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,94 @@
+import pygame
+import random
+import pendulum
+
+
+def init():
+    global window
+
+    pygame.init()
+    window = pygame.display.set_mode((800, 500))
+    pygame.display.set_caption("Double Pendulum Simulation")
+
+
+def find_colour(total_num, current_num):
+    if total_num <= 3:
+        colour = [0, 0, 0]
+        colour[current_num] = 255
+        return tuple(colour)
+    else:
+        return ((current_num * 20) % 255, (current_num * 20) % 255, (current_num * 20) % 255)
+
+
+def create_single(mass_upper, mass_lower, length_upper, length_lower, angle_upper, angle_lower, colour):
+    global pendulums
+
+    upper_anchor = (400, 100)
+    p = pendulum.Pendulum(mass_upper, mass_lower, upper_anchor, angle_upper, angle_lower, length_upper, length_lower, 15, colour, window)
+    pendulums = [p]
+
+
+def vary_mass(num, length_upper, length_lower, start_angle):
+    global pendulums
+
+    pendulums = []
+    upper_anchor = (400, 100)
+
+    for i in range(num):
+        colour = find_colour(num, i)
+        
+        new = pendulum.Pendulum(random.uniform(4.9, 5.1), random.uniform(1.9, 2.1), upper_anchor, start_angle, start_angle, length_upper, length_lower, 15, colour, window)
+        pendulums.append(new)
+
+
+def vary_start_pos(num, length_upper, length_lower, mass_upper, mass_lower):
+    global pendulums
+
+    pendulums = []
+    upper_anchor = (400, 100)
+
+    for i in range(num):
+        colour = find_colour(num, i)
+        
+        new = pendulum.Pendulum(mass_upper, mass_lower, upper_anchor, random.uniform(89.9, 90.1), random.uniform(89.9, 90.1), length_upper, length_lower, 15, colour, window)
+        pendulums.append(new)
+
+
+def vary_length(num, mass_upper, mass_lower, start_angle):
+    global pendulums
+
+    pendulums = []
+    upper_anchor = (400, 100)
+
+    for i in range(num):
+        length_upper = random.uniform(140, 150)
+        length_lower = random.uniform(65, 75)
+        
+        colour = find_colour(num, i)
+        
+        new = pendulum.Pendulum(mass_upper, mass_lower, upper_anchor, start_angle, start_angle, length_upper, length_lower, 15, colour, window)
+        pendulums.append(new)
+
+
+def main():
+    while True:
+        pygame.time.Clock().tick(60)
+        window.fill((0, 0, 0))
+        
+        for i in pendulums:
+            if pygame.mouse.get_pressed(3)[0]:
+                i.check_click()
+
+            i.main()
+
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                quit()
+
+        pygame.display.update()
+
+
+init()
+#create_single(3, 2, 150, 75, 40, 30, (255, 255, 255))
+vary_start_pos(3, 150, 75, 5, 5)
+main()
---
+++ pendulum.py
@@ -0,0 +1,107 @@
+import pygame
+import math
+
+
+G = 9.81
+SHOW_PATH = True
+
+
+class Pendulum:
+    def __init__(self, mass_upper, mass_lower, upper_anchor, theta1, theta2, length_upper, length_lower, radius, colour, window):
+        self.m1 = mass_upper
+        self.m2 = mass_lower
+        self.upper_anchor = upper_anchor
+        self.l1 = length_upper
+        self.l2 = length_lower
+        self.radius = radius
+        self.colour = colour
+        self.window = window
+
+        self.dt = 0.1
+        self.ang_vel_u = 0
+        self.ang_vel_l = 0
+        self.positions = []
+
+        self.theta = theta1
+        self.theta2 = theta2
+
+    def find_angle(self, pos1, pos2):
+        x = pos1[0] - pos2[0]
+        y = pos1[1] - pos2[1]
+
+        return math.atan(x / y)
+
+    def find_pos(self, angle, length, anchor_pos):
+        x = length * math.sin(angle)
+        y = length * math.cos(angle)
+
+        return (int(anchor_pos[0] + x), int(anchor_pos[1] + y))
+
+    def force_upper(self):
+        numerator1 = -G * (2 * self.m1 + self.m2) * math.sin(self.theta) - self.m2 * G * math.sin(self.theta - 2 * self.theta2)
+        numerator2 = 2 * math.sin(self.theta - self.theta2) * self.m2 * (self.ang_vel_l**2 * self.l2 + self.ang_vel_u**2 * self.l1 * math.cos(self.theta - self.theta2))
+        numerator = numerator1 - numerator2
+        denominator = self.l1 * (2 * self.m1 + self.m2 - self.m2 * math.cos(2 * self.theta - 2 * self.theta2))
+        
+        self.ang_vel_u += numerator / denominator * self.dt
+
+    def force_lower(self):
+        numerator = 2 * math.sin(self.theta - self.theta2) * (self.ang_vel_u**2 * self.l1 * (self.m1 + self.m2) + G * (self.m1 + self.m2) * math.cos(self.theta) + self.ang_vel_l**2 * self.l2 * self.m2 * math.cos(self.theta - self.theta2))
+        denominator = self.l2 * (2 * self.m1 + self.m2 - self.m2 * math.cos(2 * self.theta - 2 * self.theta2))
+
+        self.ang_vel_l += numerator / denominator * self.dt
+
+    def draw_pendulum(self):
+        pygame.draw.circle(self.window, self.colour, self.upper_pos, self.radius)
+        pygame.draw.circle(self.window, self.colour, self.lower_pos, self.radius)
+
+        pygame.draw.line(self.window, self.colour, self.upper_anchor, self.upper_pos, 2)
+        pygame.draw.line(self.window, self.colour, self.upper_pos, self.lower_pos, 2)
+
+    def draw_path(self):
+        for i, x in enumerate(self.positions):
+            if i > 0:
+                pygame.draw.line(self.window, self.colour, x, self.positions[i - 1], 2)
+
+    def check_click(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+
+        if self.upper_pos[0] - self.radius - 10 <= mouse_x <= self.upper_pos[0] + self.radius + 10 and self.upper_pos[1] - self.radius - 10 <= mouse_y <= self.upper_pos[1] + self.radius + 10:
+            self.move_to_mouse(True, pygame.mouse.get_pos())
+        elif self.lower_pos[0] - self.radius - 10 <= mouse_x <= self.lower_pos[0] + self.radius + 10 and self.lower_pos[1] - self.radius - 10 <= mouse_y <= self.lower_pos[1] + self.radius + 10:
+            self.move_to_mouse(False, pygame.mouse.get_pos())
+
+    def move_to_mouse(self, is_upper, mouse_pos):
+        x = mouse_pos[0]
+
+        if is_upper and x - self.upper_anchor[0] < self.l1:
+            y = int(math.sqrt(self.l1**2 - (x - self.upper_anchor[0])**2) + self.upper_anchor[1])
+            self.upper_pos = [x, y]
+            self.theta = self.find_angle(self.upper_anchor, self.upper_pos)
+        elif x - self.upper_pos[0] < self.l2:
+            y = int(math.sqrt(self.l2**2 - (x - self.upper_pos[0])**2) + self.upper_pos[1])
+            self.lower_pos = [x, y]
+            self.theta2 = self.find_angle(self.upper_pos, self.lower_pos)
+
+        self.ang_vel_u = 0
+        self.ang_vel_l = 0
+
+    def main(self):
+        self.force_upper()
+        self.force_lower()
+
+        new_u = self.ang_vel_u * self.dt
+        new_l = self.ang_vel_l * self.dt
+
+        self.theta += new_u
+        self.theta2 += new_l
+
+        self.upper_pos = self.find_pos(self.theta, self.l1, self.upper_anchor)
+        self.lower_pos = self.find_pos(self.theta2, self.l2, self.upper_pos)
+
+        self.positions.append(self.lower_pos)
+
+        self.draw_pendulum()
+
+        if SHOW_PATH:
+            self.draw_path()
